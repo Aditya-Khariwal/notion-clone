77da9e45af3ef97b841fe3e00fe59b793d2bab27
Aditya <adityakhariwal2003@gmail.com>
2024-12-07 13:05:06 +0530

    Fixed stuff 7

--- app/doc/[id]/layout.tsx
+++ app/doc/[id]/layout.tsx
@@ -2,14 +2,14 @@ import RoomProvider from "@/components/RoomProvider"
 import { auth } from "@clerk/nextjs/server"
 import React from "react"
 
-type Params =  Promise<{ id: string }>
+type Params = { id: string };
 async function Doclayout({ children, params }: {
   children: React.ReactNode
   params: Params
 }) {
   const { userId, redirectToSignIn } = await auth()
   if (!userId) return redirectToSignIn()
-  const {id} = await params
+  const {id} = params
   return (
 
     <RoomProvider roomId={id}>
