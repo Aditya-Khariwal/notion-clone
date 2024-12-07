d70d13e7afb42ddf13ef174d5fd8e48dfe5259b3
Aditya <adityakhariwal2003@gmail.com>
2024-12-07 14:49:12 +0530

    Fixed stuff 10

--- app/doc/[id]/layout.tsx
+++ app/doc/[id]/layout.tsx
@@ -2,14 +2,16 @@ import RoomProvider from "@/components/RoomProvider"
 import { auth } from "@clerk/nextjs/server"
 import React from "react"
 
-type Params = { id: string };
+type Params =Promise<{ id: string } >;
+
+
 async function Doclayout({ children, params }: {
   children: React.ReactNode
-  params: Promise<Params>
+  params: Params
 }) {
   const { userId, redirectToSignIn } = await auth()
   if (!userId) return redirectToSignIn()
-  const id = (await params).id
+  const {id} = await params
   return (
 
     <RoomProvider roomId={id}>
--- app/doc/[id]/page.tsx
+++ app/doc/[id]/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import Document from "@/components/Document";
+import { use } from 'react'
+type Params =Promise<{ id: string } >;
 
-function DocumentPage({params:{id}}: {
-    params:{
-        id: string;
-    }
+function DocumentPage({params}: {
+      params: Params
 }) {
+  const id = use(params).id
   return (
     <div className="flex flex-col flex-1 min-h-screen">
       <Document id={id}/>
